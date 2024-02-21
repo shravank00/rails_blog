19fc66db027f0749158c1207402bb34027cf1ecf
Shravan <chaudharysharwan2000@gmail.com>
2024-02-21 18:50:51 +0530

    Added Post CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
 	def index
 		@posts = Post.all
 	end
@@ -6,6 +7,8 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new()
 	end
+
+	def show; end
 
 	def create
 		@post = Post.new(post_params)
@@ -16,7 +19,26 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def edit;	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render :edit, status: :unprocessable_entity
+		end
+	end
+	
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
 	private
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
 
 	def post_params
 		params.require(:post).permit(:title, :description)
